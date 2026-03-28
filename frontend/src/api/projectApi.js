a4ec6255f7d2a3984cd0cf2dd601feeaef7efe2a
Saksham Varshney <sakshamvarshney43@gmail.com>
2026-03-28 12:30:43 +0530

    projectApi

---
+++ frontend/src/api/projectApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const API = "http://localhost:5000/api";
+
+//GET all projects
+export async function getAllProjects() {
+  const { data } = await axios.get(`${API}/projects`);
+  return data;
+}
+
+//get Single project by id
+export async function getProjectById(id) {
+  const { data } = await axios.get(`${API}/projects/${id}`);
+  return data;
+}
+
+//Save a new Project
+export async function saveProject(payload) {
+  const { data } = await axios.post(`${API}/projects`, payload);
+  return data;
+}
+
+//Delete a project
+export async function deleteProject(id) {
+  const { data } = await axios.delete(`${API}/projects/${id}`);
+  return data;
+}
